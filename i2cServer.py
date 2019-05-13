af9fb8d3b0ac4a6edfaeb6750b854974d24b34ef
Ricardo Correia <ricardo.correia@petapilot.com>
2019-05-13 21:09:26 +0100

    Formatted Code

--- i2cServer.py
+++ i2cServer.py
@@ -5,8 +5,7 @@ import os
 pi = None
 slave_addr = 0x04
 
-print
-"Listening..."
+print("Listening...")
 
 
 def i2cInterrupt(id, tick):
@@ -15,9 +14,9 @@ def i2cInterrupt(id, tick):
     status, bytes_read, data = pi.bsc_i2c(slave_addr)  # pi.bsc_i2c(slave_addr, data=" ACK")
     if bytes_read:
         if data == "OK":
-            print
-            "Message:"
-    # print(data[:-1])
+            print("Message:")
+            print(data[:-1])
+
 
 pi = pigpio.pi()  # inicia a conexao
 int_handler = pi.event_callback(pigpio.EVENT_BSC, i2cInterrupt)

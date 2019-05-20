fb606d51ccfceaa63d1038cdcf1eca04b6d60913
Ricardo Correia <ricardo.correia@petapilot.com>
2019-05-20 19:07:50 +0100

    Added i2c gateway

---
+++ I2cToMqttGateway.py
@@ -0,0 +1,10 @@
+import python_i2c_estg as i2c
+import python_mqtt_estg as mqtt
+
+
+def message_handler(client, userdata, i2c_message):
+    print(i2c_message)
+    mqtt.publish(message=i2c_message)
+
+
+i2c.connect(0x04, message_handler, True)
